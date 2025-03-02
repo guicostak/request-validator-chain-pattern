15545dcbee97271c877e1da630f1b7beb129dc94
Guilherme Augustto <guilhermecosta.barros@gmail.com>
2025-03-02 20:01:51 -0300

    fix: removing list of violations from class

--- src/main/java/com/example/validator/validator/ValidatorChain.java
+++ src/main/java/com/example/validator/validator/ValidatorChain.java
@@ -8,13 +8,14 @@ import java.util.List;
 public class ValidatorChain<T> {
 
     private final List<Validator<T>> validadores = new ArrayList<>();
-    private List<Violacao> violacoes = new ArrayList<>();
+
 
     public void adicionarValidador(Validator<T> validador) {
         validadores.add(validador);
     }
 
     public List<Violacao> validar(T objeto) {
+        List<Violacao> violacoes = new ArrayList<>();
         validadores.forEach(validador -> validador.validate(objeto, violacoes));
 
         return violacoes;
